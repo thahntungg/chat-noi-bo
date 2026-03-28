55903282cec30332ba9d26c984e948f2ad29047a
Thanh Tùng <dangthanhtung020312@gmail.com>
2026-03-28 20:44:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import eventlet
-eventlet.monkey_patch()
+from gevent import monkey
+monkey.patch_all() # Phải nằm trên cùng
 
 import os
 from flask import Flask, render_template
@@ -7,10 +7,10 @@ from flask_socketio import SocketIO, emit
 import emoji
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'chat8a_2026_pro'
+app.config['SECRET_KEY'] = 'chat8a_2026_final'
 
-# Cấu hình SocketIO tối ưu cho môi trường Web
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+# Cấu hình SocketIO dùng gevent
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
 @app.route('/')
 def index():
@@ -18,14 +18,12 @@ def index():
 
 @socketio.on('message')
 def handle_message(data):
-    # data: {'text': '...', 'sender_name': '...'}
     if data and 'text' in data:
-        # Chuyển đổi emoji (ví dụ :smile: -> 😄)
         msg_unicode = emoji.emojize(data['text'], language='alias')
-        
+        # Gửi dữ liệu đi kèm tên người gửi
         emit('render_message', {
             'text': msg_unicode,
-            'sender_name': data.get('sender_name', 'Thành viên 8A')
+            'sender_name': data.get('sender_name', 'Bạn học 8A')
         }, broadcast=True)
 
 if __name__ == '__main__':
